4503830d2e18969c2faa5f8439496347da3653df
alpertunga-bile <kingdom19online@hotmail.com>
2025-02-10 23:55:14 +0300

    config reading is added

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 mod civitai_image_struct;
+mod config;
 
 use civitai_image_struct::CivitaiImagePage;
+use config::{from_config_to_url, AutoCivitaiConfig};
 use reqwest::get;
+use std::fs;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = get("https://civitai.com/api/v1/images")
+async fn fetch_data() -> Result<CivitaiImagePage, Box<dyn std::error::Error>> {
+    let body: CivitaiImagePage = get("https://civitai.com/api/v1/images")
         .await?
-        .text()
+        .json::<CivitaiImagePage>()
         .await?;
 
-    let result: CivitaiImagePage = serde_json::from_str(&body)?;
+    Ok(body)
+}
 
-    println!("{}", result.metadata.nextCursor.clone().unwrap());
+use std::io::{Error, ErrorKind};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let config_file: String = String::from("config.json");
+
+    let is_config_exists: bool = fs::exists(config_file.clone()).is_ok();
+
+    if !is_config_exists {
+        return Err(Error::new(
+            ErrorKind::NotFound,
+            "config.json file is not exists",
+        ));
+    }
+
+    let config: AutoCivitaiConfig =
+        serde_json::from_str(fs::read_to_string(config_file).unwrap().as_str()).unwrap();
+
+    println!("{}", config.period);
 
     Ok(())
 }
